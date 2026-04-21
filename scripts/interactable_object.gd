e0547ca29313e8517aec993d2cfc30904264c5ab
vadim <vadim.ugatu@mail.ru>
2026-04-21 17:18:55 +0500

    feat: add cursor change on hover over interactive object

--- scripts/interactable_object.gd
+++ scripts/interactable_object.gd
@@ -16,10 +16,12 @@ func _ready():
 func _on_mouse_enter():
 	is_hovered = true
 	$Sprite2D.texture = hover_texture
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 
 func _on_mouse_exit():
 	is_hovered = false
 	$Sprite2D.texture = default_texture
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
