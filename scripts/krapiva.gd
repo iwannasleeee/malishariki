c43596bb7d2e16e18f62a957e1be7aa0b6f6642c
vadim <vadim.ugatu@mail.ru>
2026-06-15 08:12:39 +0500

    feat: add minimally working version of Krapiva minigame with Nettle and Flower objects

--- scripts/krapiva.gd
+++ scripts/krapiva.gd
@@ -2,6 +2,7 @@ extends Control
 
 enum FLOWERS_NAME {ANEMONE, LUTIK, TULIP, VASILEK}
 @onready var flowers = $Flowers
+@onready var nettles = $Nettles
 
 var win_condition: Dictionary = {
 	FLOWERS_NAME.ANEMONE: 4,
@@ -21,11 +22,16 @@ var nettle_count = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	flowers_connect()
+	krapiva_connect()
 
 func flowers_connect():
 	for child in flowers.get_children():
 		if child.has_signal("pick_me"):
 			child.pick_me.connect(_collect_flower)
+func krapiva_connect():
+	for child in nettles.get_children():
+		if child.has_signal("nettle_toched"):
+			child.nettle_toched.connect(_touch_nettle)
 
 func _collect_flower(flower):
 	if not collected_flowers.has(flower):
@@ -35,12 +41,22 @@ func _collect_flower(flower):
 	
 	if _is_win_condition():
 		EventBus.minigame_finished.emit({})
-		
+		UIManager.hide_minigame()
+
 func _is_win_condition():
 	for flower in win_condition:
 		if win_condition[flower] > collected_flowers[flower]:
 			return false
 	return true
+func _touch_nettle():
+	nettle_count += 1
+	_show_ai()
+	if nettle_count >= 3:
+		EventBus.minigame_finished.emit({})
+		UIManager.hide_minigame()
+func _show_ai():
+	print("АЙ!!!!!!")
+	#Здесь добавить лейблы с количеством цветов и обжогов
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
