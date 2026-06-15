4879e7c5a37131a903fb5e5d330fb65bf93aa593
vadim <vadim.ugatu@mail.ru>
2026-06-15 10:03:29 +0500

    chore: remove bugged scenes that hindered development

---
+++ scripts/kr_flower.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+enum FLOWERS_NAME {ANEMONE, LUTIK, TULIP, VASILEK}
+@export var flower_name: FLOWERS_NAME 
+var is_taken = false 
+signal pick_me(flower: FLOWERS_NAME)
+
+func _ready() -> void:
+	# Подключаем сигнал input_event к функции внутри этого же скрипта
+	input_event.connect(_on_input_event)
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	# Проверяем, что событие — это нажатие кнопки мыши
+	if event is InputEventMouseButton:
+		# Проверяем, что нажата именно ЛЕВАЯ кнопка мыши и она НАЖАТА (а не отпущена)
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			_on_object_clicked()
+
+func _on_object_clicked() -> void:
+	is_taken = true
+	pick_me.emit(flower_name)
+	visible = false
