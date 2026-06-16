01a652529f856d04c10d2be411d7a685496c4b9d
vadim <vadim.ugatu@mail.ru>
2026-06-17 04:30:50 +0500

    feat: add dialog on load

--- scripts/forest_path.gd
+++ scripts/forest_path.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
 @export var player_scale = 1.0
+var on_location_load_dialogue = [
+		{
+			"name": "Киря",
+			"text": "Теперь можно садиться за плакат!!",
+			"portrait": "res://scenes/ui/dialogue/portraits/LiniPortrait.tscn",
+			"animation": "talk"        # имя анимации в SpriteFrames
+		},
+	]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SceneManager.player.setup_for_location({
 		"state": "mini",
 		"scale": player_scale
 	})
+	EventBus.scene_became_visible.connect(_on_location_load)
+
+func _on_location_load():
+	EventBus.dialogue_started.emit("res://scenes/ui/dialogue/Dialogue.tscn", on_location_load_dialogue)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
